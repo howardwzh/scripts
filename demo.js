4c19352462245105d05c9c65608471cb0d02d582
wangzhonghua <wangzhonghua@gzjkp2p.com>
2022-01-12 04:42:30 +0800

    demo21

--- demo.js
+++ demo.js
@@ -23,29 +23,45 @@
       <p><label>Max Price: <input type="text" id="maxPrice77"/></label></p>
       <p><label>Min Price: <input type="text" id="minPrice77"/></label></p>
       <p><label>Warning Offset: <input type="text" id="warningOffset77"/></label></p>
+      <p style="margin-top: 20px; text-align: center;"><button id="toggleWarning77">开始监听</button></p>
     </div>
   `
   document.body.appendChild(panel.children[0]);
 
   sendPriceNotification();
 
-  // 每秒检测价格
-  setInterval(() => {
-    const lastPrice = Number(priceDom.innerText||0)
-    const maxPrice = Number(document.getElementById('maxPrice77').value || 7777777)
-    const minPrice = Number(document.getElementById('minPrice77').value || 0)
-    const warningOffset = Number(document.getElementById('warningOffset77').value || 0.005)
-
-    if (lastPrice > maxPrice) {
-      sendPriceNotification('价格 超出 大值！')
-    } else if (lastPrice < minPrice) {
-      sendPriceNotification('价格 低于 小值！')
-    } else if (lastPrice >= maxPrice - warningOffset) {
-      sendPriceNotification('价格 接近 大值！')
-    } else if (lastPrice <= minPrice + warningOffset) {
-      sendPriceNotification('价格 接近 小值！')
+  // 设置监听事件
+  let inter
+  const maxPrice77 = document.getElementById('maxPrice77');
+  const minPrice77 = document.getElementById('minPrice77');
+  const warningOffset77 = document.getElementById('warningOffset77');
+  const toggleWarning77 = document.getElementById('toggleWarning77');
+  const seconds = 3
+  toggleWarning77.addEventListener('click', () => {
+    const text = toggleWarning77.innerText
+    if (text === '开始监听') {
+      inter = setInterval(() => {
+        const lastPrice = Number(priceDom.innerText || 0)
+        const maxPrice = Number(maxPrice77.value || 7777777)
+        const minPrice = Number(minPrice77.value || 0)
+        const warningOffset = Number(warningOffset77.value || 0.005)
+    
+        if (lastPrice > maxPrice) {
+          sendPriceNotification('价格 超出 大值！')
+        } else if (lastPrice < minPrice) {
+          sendPriceNotification('价格 低于 小值！')
+        } else if (lastPrice >= maxPrice - warningOffset) {
+          sendPriceNotification('价格 接近 大值！')
+        } else if (lastPrice <= minPrice + warningOffset) {
+          sendPriceNotification('价格 接近 小值！')
+        }
+      }, seconds * 1000);
+      toggleWarning77.innerText = '停止监听'
+    } else {
+      clearInterval(inter)
+      toggleWarning77.innerText = '开始监听'
     }
-  }, 1000);
+  })
 
   function sendPriceNotification(msg) {
     if (Notification.requestPermission) {
