3a425fcd874bdc2c2d754d7f78a435cabe728b17
wangzhonghua <wangzhonghua@gzjkp2p.com>
2022-01-12 02:19:30 +0800

    demo2

--- demo.js
+++ demo.js
@@ -1,1 +1,24 @@
-alert(666)
+window.onload = () => {
+  const priceDom = document.getElementById('_spanLastPrice');
+  if (!priceDom) return
+  priceDom.style.position = 'fixed'
+  priceDom.style.zIndex = 7777777
+  priceDom.style.left = '27px'
+  priceDom.style.top = '27px'
+  priceDom.style.color = '#777'
+
+  const panel = document.createElement('div');
+  panel.innerHTML = `
+    <div style="
+      position: fixed; 
+      top: 0; 
+      bottom: 0; 
+      left: 0; 
+      right:0;  
+      background-color: #fff; 
+      z-index:777777
+      "
+    />
+  `
+  document.body.appendChild(panel.children[0]);
+}
