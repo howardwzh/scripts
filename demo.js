9b7fbe514445c3a7f3b94a7d7a722a0e5af35cb7
wangzhonghua <wangzhonghua@gzjkp2p.com>
2022-01-12 03:04:06 +0800

    demo6

--- demo.js
+++ demo.js
@@ -20,12 +20,14 @@
       padding: 100px 27px;
       "
     >
-    <p><label>Max Price: <input type="text" id="maxPrice77"/></label></p>
-    <p><label>Min Price: <input type="text" id="minPrice77"/></label></p>
-    <p><label>Warning Offset: <input type="text" id="warningOffset77"/></label></p>
+      <p><label>Max Price: <input type="text" id="maxPrice77"/></label></p>
+      <p><label>Min Price: <input type="text" id="minPrice77"/></label></p>
+      <p><label>Warning Offset: <input type="text" id="warningOffset77"/></label></p>
     </div>
   `
   document.body.appendChild(panel.children[0]);
+
+  sendNotification();
 
   // 每秒检测价格
   setInterval(() => {
@@ -43,12 +45,12 @@
     } else if (lastPrice <= minPrice + warningOffset) {
       sendNotification('价格 接近 小值！')
     }
-  }, 1000)
-})()
+  }, 1000);
 
-function sendNotification(msg) {
-  Notification.requestPermission( function(status) {
-    console.log(status); // 仅当值为 "granted" 时显示通知
-    var n = new Notification("消息", {body: msg || "已经开启消息通知！"}); // 显示通知
-  });
-}
+  function sendNotification(msg) {
+    Notification.requestPermission( function(status) {
+      console.log(status); // 仅当值为 "granted" 时显示通知
+      var n = new Notification("消息", {body: msg || "已经开启消息通知！"}); // 显示通知
+    });
+  };
+})()
