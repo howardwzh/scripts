3522654e0ca4ee33d9eb82d6a9b556380cbbe2bd
wangzhonghua <wangzhonghua@gzjkp2p.com>
2022-01-17 15:38:21 +0800

    demo54

--- demo.js
+++ demo.js
@@ -32,7 +32,7 @@
           <p id="buyPriceItem"><label>Buy: <input type="text" id="buyPriceInput"/></label></p>
           <p id="debugMsg"></p>
           <p id="monitorRemark" style="padding: 12px 0; background-color: #ddd;">备注</p>
-          <textarea id="editMonitorRemark" style="display: none;font-size: 14px;" row="5" />
+          <textarea id="editMonitorRemark" style="display: none;font-size: 14px;" rows="5" />
         </div>
         <button id="toggleBtn" style="position: fixed; z-index: 7777777; bottom: 77px; right: 27px; font-size: 14px; padding: 7px 14px;">开关</button>
       </div>
