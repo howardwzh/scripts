5116557281101da0ea72886e357630d070deb4e8
wangzhonghua <wangzhonghua@gzjkp2p.com>
2022-02-16 12:32:31 +0800

    demo: 调整字体大小

--- demo.js
+++ demo.js
@@ -33,15 +33,15 @@
           "
         >
           <div id="monitorHistory" style="font-size: 14px; text-align: left; position: absolute; top: 27px;"></div>
-          <p style="margin:16px 0 0"><label><span>SellPrice：</span><textarea style="font-size: 14px;width: 100%;min-height: 77px" rows="5" id="sellPriceInput"></textarea></label></p>
-          <p style="margin:16px 0 0"><label>Total Number:<span id="totalNumberBox" style="margin-right: 20px"></span>Total Money:<span id="totalMoneyBox"></span></label></p>
+          <p style="margin:16px 0 0"><label><span>SellPrice：</span><textarea style="width: 100%;min-height: 77px;font-size: 14px;" rows="5" id="sellPriceInput"></textarea></label></p>
+          <p style="margin:0"><label>Total Number:<span id="totalNumberBox" style="margin-right: 20px"></span>Total Money:<span id="totalMoneyBox"></span></label></p>
           <p style="margin:54px 0 0"><label><span>BuyPrice：</span><input style="width: 100%; min-height: 28px" type="text" id="buyPriceInput"/></label><span id="winNumber"></span></p>
           <p id="suggestPriceListDom" style="word-break: break-all;text-align: left;"></p>
           <p id="debugMsg"></p>
           <div id="monitorRemark" style="padding: 12px 0; text-align: left;">备注</div>
           <textarea id="monitorEditRemark" style="display: none;font-size: 14px; width: 100%;" rows="5"></textarea>
         </div>
-        <button id="toggleBtn" style="position: fixed; z-index: 7777777; width: 54px; height: 54px; opacity: 0.2; top: 382px; left: 50%; margin-left: -27px; font-size: 14px; padding: 7px 14px;"></button>
+        <button id="toggleBtn" style="position: fixed; z-index: 7777777; width: 54px; height: 54px; opacity: 0.2; top: 310px; left: 50%; margin-left: -27px; font-size: 14px; padding: 7px 14px;"></button>
       </div>
     `;
     document.body.appendChild(panel.children[0]);
